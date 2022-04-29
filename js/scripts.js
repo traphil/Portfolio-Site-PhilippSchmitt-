982862bae8cbb887e0a04275d2743448e8314a82
Anil <anilbms75@gmail.com>
2022-04-29 20:43:24 +0200

    remove unused js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-console.log("Hey! Welcome to My Portfolio Site");
 function menuToggle() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle") {
@@ -7,44 +6,3 @@ function menuToggle() {
     x.className = "navtoggle";
   }
 }
-
-
-var Integer = 456;
-var Integer2 = -345; // This is negative integer
-var Float = 50.25;
-
-var boolean1 = true;
-var boolean2 = false;
-var notABoolean = "false"; //This is just the string "false", not false!
-
-var emptyVariable = null;
-var notNullButAString = "null";
-console.log(doesntExist); // Outputs undefined
-console.log(emptyVariable); // Outputs Null
-console.log(notNullButAString); // Outputs "null" - not the same as null!
-
-
-    var age = 39;
-    var name = "John";
-    var personParent = {
-      name: name,
-      age: age,
-      child: {
-        name: "Sara"
-      }
-    };
-
-personParent.name = "Anne"; // Set the name property to "Anne"
-personParent.child.size = 175; // Set new values that weren't set before
-console.log(personParent.child.name); // Output "Sara" to the console
-
-console.log(personParent["name"]); // The same as personParent.name
-var weWantThisField = "age";
-  console.log(personParent[weWantThisField]); // The same as personParent.age
-
-delete shortObject.name; // Will remove the "name" property
-
-
-//this
-
-console.log(this)
